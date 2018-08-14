d6bb5abcd86fdbe8ce3d574aa4d7c36e820a63bb
wuyou <wuyou331@gmail.com>
2018-08-14 11:54:05 +0800

    代码整理

--- CoreScript/ScriptEngine.cs
+++ CoreScript/ScriptEngine.cs
@@ -15,7 +15,6 @@ namespace CoreScript
         /// </summary>
         public IDictionary<string, ScriptFunction> Functions => _functions;
 
-    
 
         public void Excute(string script)
         {
@@ -23,13 +22,11 @@ namespace CoreScript
             var stack = new VariableStack();
             //初始化全局变量
             foreach (var token in rs.Where(it => it.TokenType == TokenType.AssignmentDefine).Cast<TokenAssignment>())
-            {
-                ExcuteAssignment(token,stack);
-            }
+                ExcuteAssignment(token, stack);
             //获取所有函数定义
             _functions = rs.Where(it => it.TokenType == TokenType.FunctionDefine)
                 .Cast<TokenFunctionDefine>()
-                .Select(it => new ScriptFunction(it,this))
+                .Select(it => new ScriptFunction(it, this))
                 .ToDictionary(it => it.Name, it => it);
 
             if (!_functions.ContainsKey("main"))
@@ -38,9 +35,6 @@ namespace CoreScript
             var main = Functions["main"];
             main.Excute(stack);
         }
-
-
-
 
 
         /// <summary>
@@ -53,59 +47,56 @@ namespace CoreScript
             if (stement.Left is TokenVariableDefine define)
             {
                 varItem = new ScriptValue();
-                stack.Push(define.Variable,varItem);
+                stack.Push(define.Variable, varItem);
             }
             else if (stement.Left is TokenVariableRef varRef)
             {
-                varItem = stack.Get(varRef.Variable) ;
+                varItem = stack.Get(varRef.Variable);
             }
 
-            var right = ReturnValue(stement.Right,stack);
+            var right = ReturnValue(stement.Right, stack);
             varItem.DataType = right.DataType;
             varItem.Value = right.Value;
         }
 
         /// <summary>
-        /// 取值
+        ///     取值
         /// </summary>
         /// <param name="value"></param>
         /// <returns></returns>
         internal static ScriptValue ReturnValue(IReturnValue value, VariableStack stack)
         {
             if (value is TokenLiteral literal)
-            {
-                return new ScriptValue() { DataType = literal.DataType, Value = literal.Value };
-            }
-            else if (value is TokenVariableRef varRef)
-            {
-                return stack.Get(varRef.Variable);
-            }
-            else if (value is TokenJudgmentExpression expr)
+                return new ScriptValue {DataType = literal.DataType, Value = literal.Value};
+
+            if (value is TokenVariableRef varRef) return stack.Get(varRef.Variable);
+
+            if (value is TokenJudgmentExpression expr)
             {
                 var left = ReturnValue(expr.Left, stack);
                 var right = ReturnValue(expr.Right, stack);
-                return new ScriptValue()
+                return new ScriptValue
                 {
                     DataType = nameof(Boolean),
                     Value = left.Value.Equals(right.Value)
                 };
-            }else if (value is TokenBinaryExpression binExpr)
-            {
-                return SumBinaryExpression(binExpr, stack);
             }
+
+            if (value is TokenBinaryExpression binExpr) return SumBinaryExpression(binExpr, stack);
 
             throw new Exception("不支持的取值方式.");
         }
 
-        private static ScriptValue SumBinaryExpression(TokenBinaryExpression expr,VariableStack stack)
+        private static ScriptValue SumBinaryExpression(TokenBinaryExpression expr, VariableStack stack)
         {
-            var left = ReturnValue(expr.Left,stack);
+            var left = ReturnValue(expr.Left, stack);
             var right = ReturnValue(expr.Right, stack);
             var rs = new ScriptValue();
 
             switch (expr.Operator)
             {
                 #region 二元运算
+
                 case '+':
                     if (left.DataType == ScriptType.Int && right.DataType == ScriptType.Int)
                     {
@@ -114,95 +105,98 @@ namespace CoreScript
                     }
                     else if (left.DataType == ScriptType.Int && right.DataType == ScriptType.Double)
                     {
-                        rs.Value = (int)left.Value + (double)right.Value;
+                        rs.Value = (int) left.Value + (double) right.Value;
                         rs.DataType = ScriptType.Double;
                     }
                     else if (left.DataType == ScriptType.Double && right.DataType == ScriptType.Double)
                     {
-                        rs.Value = (double)left.Value + (int)right.Value;
+                        rs.Value = (double) left.Value + (int) right.Value;
                         rs.DataType = ScriptType.Double;
                     }
                     else if (left.DataType == ScriptType.String || right.DataType == ScriptType.String)
                     {
-                        rs.Value = left.Value.ToString() + right.Value.ToString();
+                        rs.Value = left.Value + right.Value.ToString();
                         rs.DataType = ScriptType.String;
                     }
+
                     break;
                 case '-':
                     if (left.DataType == ScriptType.Int && right.DataType == ScriptType.Int)
                     {
                         rs.DataType = ScriptType.Int;
-                        rs.Value = (int)left.Value - (int)right.Value;
+                        rs.Value = (int) left.Value - (int) right.Value;
                     }
                     else if (left.DataType == ScriptType.Int && right.DataType == ScriptType.Double)
                     {
-                        rs.Value = (int)left.Value - (double)right.Value;
+                        rs.Value = (int) left.Value - (double) right.Value;
                         rs.DataType = ScriptType.Double;
                     }
                     else if (left.DataType == ScriptType.Double && right.DataType == ScriptType.Double)
                     {
-                        rs.Value = (double)left.Value - (int)right.Value;
+                        rs.Value = (double) left.Value - (int) right.Value;
                         rs.DataType = ScriptType.Double;
                     }
+
                     break;
                 case '*':
                     if (left.DataType == ScriptType.Int && right.DataType == ScriptType.Int)
                     {
                         rs.DataType = ScriptType.Int;
-                        rs.Value = (int)left.Value * (int)right.Value;
+                        rs.Value = (int) left.Value * (int) right.Value;
                     }
                     else if (left.DataType == ScriptType.Int && right.DataType == ScriptType.Double)
                     {
-                        rs.Value = (int)left.Value * (double)right.Value;
+                        rs.Value = (int) left.Value * (double) right.Value;
                         rs.DataType = ScriptType.Double;
                     }
                     else if (left.DataType == ScriptType.Double && right.DataType == ScriptType.Double)
                     {
-                        rs.Value = (double)left.Value * (int)right.Value;
+                        rs.Value = (double) left.Value * (int) right.Value;
                         rs.DataType = ScriptType.Double;
                     }
+
                     break;
                 case '/':
                     if (left.DataType == ScriptType.Int && right.DataType == ScriptType.Int)
                     {
                         rs.DataType = ScriptType.Int;
-                        rs.Value = (int)left.Value / (int)right.Value;
+                        rs.Value = (int) left.Value / (int) right.Value;
                     }
                     else if (left.DataType == ScriptType.Int && right.DataType == ScriptType.Double)
                     {
-                        rs.Value = (int)left.Value / (double)right.Value;
+                        rs.Value = (int) left.Value / (double) right.Value;
                         rs.DataType = ScriptType.Double;
                     }
                     else if (left.DataType == ScriptType.Double && right.DataType == ScriptType.Double)
                     {
-                        rs.Value = (double)left.Value / (int)right.Value;
+                        rs.Value = (double) left.Value / (int) right.Value;
                         rs.DataType = ScriptType.Double;
                     }
+
                     break;
                 case '%':
                     if (left.DataType == ScriptType.Int && right.DataType == ScriptType.Int)
                     {
                         rs.DataType = ScriptType.Int;
-                        rs.Value = (int)left.Value % (int)right.Value;
+                        rs.Value = (int) left.Value % (int) right.Value;
                     }
                     else if (left.DataType == ScriptType.Int && right.DataType == ScriptType.Double)
                     {
-                        rs.Value = (int)left.Value % (double)right.Value;
+                        rs.Value = (int) left.Value % (double) right.Value;
                         rs.DataType = ScriptType.Int;
                     }
                     else if (left.DataType == ScriptType.Double && right.DataType == ScriptType.Double)
                     {
-                        rs.Value = (double)left.Value % (int)right.Value;
+                        rs.Value = (double) left.Value % (int) right.Value;
                         rs.DataType = ScriptType.Int;
                     }
+
                     break;
-#endregion
+
+                #endregion
             }
+
             return rs;
         }
-
-
-
-
     }
 }
