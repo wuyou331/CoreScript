ed444cdadee0dd38092a677e373301fa3b8f5ecb
wuyou <wuyou331@gmail.com>
2018-08-30 15:34:43 +0800

    代码整理

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,7 +8,35 @@ namespace ConsoleApp
     {
         private static void Main(string[] args)
         {
-            var script = File.ReadAllText("script.cj");
+            var script = "var age=18;" +
+                         "func main(){" +
+                         "    var name = getName();" +
+                         "    print(\"my name is \",false);" +
+                         "    println(name);  " +
+                         "    if false then{" +
+                         "        name = name;  " +
+                         "    } else {" +
+                         "        name = (12+2)*3+456+\"haha\";" +
+                         "        println(name); " +
+                         "        name= \"first code\";  " +
+                         "    }" +
+                         "    println(getName()); " +
+                         "    println(age);" +
+                         "}" +
+                         "func print(var str,var repart){" +
+                         "    Console.Write(str);" +
+                         "    if repart==false then {" +
+                         "        return ; " +
+                         "    }" +
+                         "    Console.Write(str);" +
+                         "}" +
+                         "func println(var str){" +
+                         "    Console.WriteLine(str);" +
+                         "}" +
+                         "    " +
+                         "func getName(){" +
+                         "    return \" \\\"wuyou \\\" \";" +
+                         "}";
             var scritpEngine = new ScriptEngine();
             scritpEngine.Excute(script);
             Console.ReadLine();
